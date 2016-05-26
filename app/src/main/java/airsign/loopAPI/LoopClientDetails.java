f612d65190fa28d0e1000083fc6870322cda70ca
miguelpcm <miguelpc85@gmail.com>
2016-05-26 10:41:21 +0200

    Delete LoopClientDetails.java

--- app/src/main/java/airsign/loopAPI/LoopClientDetails.java
+++
@@ -1,9 +0,0 @@
-package airsign.loopAPI;
-
-public class LoopClientDetails
-{
-    public static final String
-        redirectUri = "airsign://loop.com",
-        clientID = "166BD33C18927BBD086A605693775675",
-        apiKey = "2D3FEECC81C11F72CC8C8201D2F3CBD1";
-}
